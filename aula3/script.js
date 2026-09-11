09d620325e801a66bdf113bcdca9c0e1ef98696e
Hebert Bratefixe Alquimim <hebert@uni9.pro.br>
2026-09-11 18:52:38 -0300

    Add files via upload

---
+++ aula3/script.js
@@ -0,0 +1,9 @@
+document.write("Externo");
+alert("Alertando!!!");
+console.log("Onde estou?");
+document.getElementById("saida").innerHTML="Falou nada!!!";
+document.getElementById("saida").innerHTML+="<hr>Nome de vcs!";
+document.querySelector(".teste").innerHTML="Classe";
+document.querySelector("#saida").innerHTML="ID";
+document.querySelector("h1").innerHTML+=" - Javascript";
+document.querySelector("h1").style.color="red";
